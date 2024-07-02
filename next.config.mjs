edaa916a9132af0c66e2460ebc9d2da3517b5449
KyawHtay <kyaw.htay@gmail.com>
2024-07-02 21:05:56 +0100

    Added deploy.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone',
+};
 
 export default nextConfig;
